28d8b9b3e4ac647d8974ba20b8af8addb019ea28
CypherXC <CypherXC@users.noreply.github.com>
2019-01-28 11:03:43 -0400

    file1

---
+++ data.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	vector<char> v; // A vector of characters (initially empty)
+	vector<int> v1 = {10, 14, 32, 64, 16}; //A vector of integers
+	
+	v.push_back('p'); // add/push a new cahracter at the back of v
+	v1.pop_back(); //remove/pop from back of vector1
+	
+	v.push_back('i');
+	v1.pop_back();
+	
+	for (int i = 0; i < 4; i++)
+	{
+		v1.push_back(0);
+	}
+	
+	for ( int i = 0; i < v1.size(); i++)
+	{
+		cout<<v1[i]<<" ";
+	}
+	
+	return 0;
+}
